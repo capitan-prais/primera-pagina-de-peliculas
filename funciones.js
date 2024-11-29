99c44bdb154ec73ace0cfef240c60c6a2b9ac2c2
Luis Cerna <luosgerardo1992@gmail.com>
2024-11-29 13:53:56 -0300

    first commit

---
+++ funciones.js
@@ -0,0 +1,37 @@
+
+//carrusel
+document.addEventListener('DOMContentLoaded', () => {
+	const elementosCarousel = document.querySelectorAll('.carousel');
+	M.Carousel.init(elementosCarousel, {
+
+		// la velocidad con que puedes mover el carrucel mas es mas lento y menos mas rapido
+		duration: 280,
+		// con este modificas el ancho total de la ruleta
+		dist: 1, 
+		// establece la distacia entre las imagenes
+		shift: 10,
+		// espaciado que llevan dentro
+ 		padding: 300,
+		// cantidad de imagenes que se muestran en la pagina
+		numVisible: 20,
+		// esto es para poner los indicadores de la ruleta
+		indicators: true,
+		// para que se cierre la ruleta con true y se repita con false
+		noWrap: false
+		
+
+	}); 
+})
+
+//menu
+	const menu = document.querySelector('.menu');
+	const btn = document.querySelector('.menu-btn');
+
+		btn.addEventListener('click', () => {
+			menu.classList.toggle('active')
+		
+		}
+		// btn.addEventListener('click', () => {
+		// 	menu.classList.toggle('pause')
+		// }
+		);
